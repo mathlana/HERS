1fa8b2195d3b13faf4c0e3a5dc202290ac9289c1
Lana Popovic <diplomiranimatematicar@gmail.com>
2023-05-13 19:24:21 +0200

    Finished first part

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.hers.pickups',
   appName: 'HERS',
   webDir: 'www',
   server: {
