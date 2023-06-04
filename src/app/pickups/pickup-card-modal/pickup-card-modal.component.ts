937dddaba34e425c5e584372300546315a64497f
Lana Popovic <diplomiranimatematicar@gmail.com>
2023-06-04 17:48:57 +0200

    App finetuning

--- src/app/pickups/pickup-card-modal/pickup-card-modal.component.ts
+++ src/app/pickups/pickup-card-modal/pickup-card-modal.component.ts
@@ -9,15 +9,15 @@ import { ModalController } from '@ionic/angular';
 })
 export class PickupCardModalComponent  implements OnInit {
 
-  @ViewChild('form', {static: true}) form: NgForm;
-  constructor(private modalCtrl: ModalController) { }
   @Input() title: string;
   @Input() status: string;
   @Input() address: string;
   @Input() city: string;
   @Input() zip: string;
   @Input() notes: string;
+  @ViewChild('form', {static: true}) form: NgForm;
 
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {}
 
